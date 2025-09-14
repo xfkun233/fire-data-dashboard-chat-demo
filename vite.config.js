ac870afb487d7aaee219363928dbe4a3f6085584
晓风君 <530621286@qq.com>
2025-09-14 22:09:13 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fire-data-dashboard-chat-demo/',  // 设置部署路径
   plugins: [
     vue(),
     vueDevTools(),
